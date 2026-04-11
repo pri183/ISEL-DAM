0c28c3ce2a5e4464ebe2150e5c914ecda7deabff
Priscila <priscilaapereira1803@gmail.com>
2026-04-11 16:18:16 +0100

    TP2- Section 1 - 1.1 feito

--- TP2/Trabalho/Kotlin/DAM_TP2/src/main/kotlin/DAM/exer_1/Exer_1.kt
+++ TP2/Trabalho/Kotlin/DAM_TP2/src/main/kotlin/DAM/exer_1/Exer_1.kt
@@ -21,13 +21,56 @@ fun List<Event>.totalSpent(username : String): Double{
     val lista_de_compras_username = this.filterIsInstance<Purchase>().filter{it.username == username}
 
     if (!lista_de_compras_username.isEmpty()){
-        return lista_de_compras_username.sumOf{it.amount}
+        spent = lista_de_compras_username.sumOf{it.amount}
     }
 
-    return spent
+    return String.format("%.2f",spent).replace(",", ".").toDouble()
 }
 
 //Passo 4
 fun processEvents(eventos: List<Event>, handler: (Event) -> Unit){
     eventos.forEach{handler(it)}
 }
+
+//Passo 5
+fun main(args: Array<String>) {
+
+    //Passo 6
+    val events = listOf(
+        Login("alice", 1_000),
+        Purchase("alice", 49.99, 1_100),
+        Purchase("bob", 19.99, 1_200),
+        Login("bob", 1_050),
+        Purchase("alice", 15.00, 1_300),
+        Logout("alice", 1_400),
+        Logout("bob", 1_500)
+    )
+
+    //Passo 5
+    processEvents(events) { when(it){
+        is Login -> println("[LOGIN]  "+it.username+" logged in at t="+it.timestamp+"")
+        is Purchase -> println("[PURCHASE]  "+it.username+" spent \$"+it.amount+" at t="+it.timestamp+"")
+        is Logout -> println("[LOGOUT]  "+it.username+"  logged out at t="+it.timestamp+"")
+    } }
+
+    //Passo 6
+    println("\nTotal spent by alice: $"+events.totalSpent("alice")+"")
+    println("Total spent by alice: $"+events.totalSpent("bob")+"")
+    println()
+
+    val eventos_alice = events.filterByUser("alice")
+    processEvents(eventos_alice) { when(it){
+        is Login -> println("Login(username="+it.username+", timestamp="+it.timestamp+")")
+        is Purchase -> println("Purchase(username="+it.username+", amount="+it.amount+", timestamp="+it.timestamp+")")
+        is Logout -> println("Logout(username="+it.username+", timestamp="+it.timestamp+")")
+    } }
+
+
+
+
+
+}
+
+
+
+
