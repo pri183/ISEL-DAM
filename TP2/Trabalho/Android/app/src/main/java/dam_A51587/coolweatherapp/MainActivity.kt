f4e45a10748e9ce744c7782470afdd8894bb8a21
Priscila <priscilaapereira1803@gmail.com>
2026-04-19 19:21:25 +0100

    TP2 - Section 2 - 2.1: Exercicio 1 e 2 da implementação do código feita

--- TP2/Trabalho/Android/app/src/main/java/dam_A51587/coolweatherapp/MainActivity.kt
+++ TP2/Trabalho/Android/app/src/main/java/dam_A51587/coolweatherapp/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.res.Configuration
+import com.google.gson.Gson
+import java.io.InputStreamReader
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,6 +48,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+
+    private fun weatherApiCall(lat: Float, long: Float): WeatherData {
+        val reqString = buildString {
+            append("https://api.open-meteo.com/v1/forecast?")
+            append("latitude=${lat}&longitude=${long}&")
+            append("current_weather=true&")
+            append("hourly=temperature_2m,weathercode,pressure_msl,windspeed_10m")
+        }
+        val str = reqString.toString()
+        val url = URL(reqString.toString());
+        url.openStream().use {
+            val request = Gson().fromJson(InputStreamReader(it,"UTF-8"),WeatherData::class.java)
+            return request
         }
     }
 }
