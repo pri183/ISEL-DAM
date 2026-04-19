df26a2e52070617a5da632acdeb432e20b04813f
Priscila <priscilaapereira1803@gmail.com>
2026-04-19 17:24:29 +0100

    TP2 - Section 2 - 2.1: Foram adicionadas novas linguas

--- TP2/Trabalho/Android/app/src/main/java/dam_A51587/coolweatherapp/MainActivity.kt
+++ TP2/Trabalho/Android/app/src/main/java/dam_A51587/coolweatherapp/MainActivity.kt
@@ -5,9 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.res.Configuration
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //Definda o booleano day-------------------------------
+        var day = true
+
+        when (resources.configuration.orientation) {
+
+
+
+            Configuration.ORIENTATION_PORTRAIT-> {
+                if (day) {
+                    println("Dia")
+                    setTheme(R.style.Theme_Day)
+                } else {
+                    println("Noite")
+                    setTheme(R.style.Theme_Night)
+                }
+            }
+            Configuration.ORIENTATION_LANDSCAPE-> {
+                if (day) {
+                    setTheme(R.style.Theme_Day_Land)
+                } else {
+                    setTheme(R.style.Theme_Night_Land)
+                }
+            }
+        }
+
+        //-------------------------------------
+
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
