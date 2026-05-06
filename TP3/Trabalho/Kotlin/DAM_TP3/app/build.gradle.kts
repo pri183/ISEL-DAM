5dfb51bd8c0f5a80b39d39fdc9c770e79738f978
Priscila <priscilaapereira1803@gmail.com>
2026-05-06 12:40:32 +0100

    TP3 - Ex1 - 1.3 - Setup feito

---
+++ TP3/Trabalho/Kotlin/DAM_TP3/app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm")
+    kotlin("kapt") // Needed for annotation processing
+}
+group = "org.example"
+version = "1.0-SNAPSHOT"
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+// Include the annotations module
+    implementation(project(":annotations"))
+// Use the annotation processor
+    kapt(project(":processor"))
+}
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(23)
+}
---
+++ TP3/Trabalho/Kotlin/DAM_TP3/settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "DAM_TP3"
+include("annotations")
+include("app")
+include("processor")
